cea1663360f843dbd538b2c24a59aefe7ca9b69a
James Medd <james.medd@sparck.io>
2024-06-18 16:46:00 +0100

    Namespace updates for CSS

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,2 +1,2 @@
 const platform = navigator.userAgent.includes('Macintosh') ? 'mac' : navigator.userAgent.includes('Windows') ? 'win' : 'other'
-document.body.classList.add(`notification-${platform}`)
+document.body.classList.add(`gukn-notification-${platform}`)

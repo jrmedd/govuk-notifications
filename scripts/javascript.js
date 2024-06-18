cd5e4ca059c3f32a2214212d4093f4bdc7b8f1c1
James Medd <james.medd@sparck.io>
2024-06-18 11:54:57 +0100

    Add Javscript, change naming convention, change messages icon

---
+++ scripts/javascript.js
@@ -0,0 +1,2 @@
+const platform = navigator.userAgent.includes('Macintosh') ? 'mac' : navigator.userAgent.includes('Windows') ? 'win' : 'other'
+document.body.classList.add(`notification-${platform}`)
